f3cbeaaec8466a9b29e019da23fd664e2dfc5485
Atul kumar tiwari <52373457+atul-77@users.noreply.github.com>
2020-07-10 12:38:41 +0530

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,28 +1,79 @@
 import heapq
 import pygame 
+from tkinter import *
 pygame.init()
+# root = Tk()
+
+start = (10,10)
+end  =  (200,200)
+
+def tkwindow():
+    root = Tk()
+    def getndestroy():
+        global start
+        global end
+        start = (int(startX.get())+10,int(startY.get())+10)
+        end = (int(endX.get())+10, int(endY.get())+10)
+        root.destroy()
+    ins = Label(root,text = "Enter valid starting and Ending coordinates in range [0,500]")
+    labelstartx = Label(root, text="Initial X coordinate:")
+    labelstarty = Label(root, text="Initial Y coordinate:")
+    labelendx = Label(root, text="Final X coordinate:")
+    labelendy = Label(root, text="Final Y coordinate:")
+    startX = Entry(root)
+    startY = Entry(root)
+    endX = Entry(root)
+    endY = Entry(root)
+    done = Button(root,text="done",command=getndestroy,width=17,bg="cyan")
+    ins.grid(row=0,column=1)
+    labelstartx.grid(row=1,column=0)
+    startX.grid(row=2,column=0)
+    labelstarty.grid(row=1,column=2)
+    startY.grid(row=2,column=2)
+
+    labelendx.grid(row=3,column=0)
+    endX.grid(row=4,column=0)
+    labelendy.grid(row=3,column=2)
+    endY.grid(row=4,column=2)
+
+    done.grid(row=5,column=2)
+    root.mainloop()
+def instruction():
+    root = Tk()
+    label = Label(root,text="Draw a grid using mouse and then press Space bar to get the result\n")
+    ok = Button(root,text = "ok" , command=root.destroy,bg="cyan")
+    label.pack()
+    ok.pack()
+    root.mainloop()
+ans = -1
+def answer():
+    global ans
+    root = Tk()
+    label = Label(root,text = "The shortest Distance is "+str(ans))
+    finish = Button(root,text="Finish",command=root.destroy,bg="cyan")
+    label.pack()
+    finish.pack()
+    root.mainloop()
 BLUE = (0,0,255)
 GREEN = (0,255,0)
 RED = (255,0,0)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 screenX,screenY = (500,500)
-start = (10,10)
-end  =  (200,190)
 def highlight(tup,color,screen):
     pygame.draw.rect(screen,color,(tup[0],tup[1],10,10))
 def dist(tup1,tup2):
     return ((tup1[0]-tup2[0])**2 + (tup1[1]-tup2[1])**2)**(1/2)
 def heuristic(to,frm):
     # dist(to,frm) +
-    return  max(abs(frm[0]-end[0]),abs(frm[1]-end[1])) #+ max(abs(frm[0]-to[0]),abs(frm[1]-to[1]))
+    return  max(abs(frm[0]-end[0]),abs(frm[1]-end[1])) + max(abs(frm[0]-to[0]),abs(frm[1]-to[1]))
     # return dist(to,frm) + dist(frm,end)
 def solution(screen):
+    global ans
     run = 1
     vis = [[False for _ in range(screenX+1)] for _ in range(screenY+1)]
     X = [(0,0,start[0],start[1])] # heur,dist
     heapq.heapify(X)
-    ans = -1
     lastx,lasty = start[0],start[1]
     while run==1 and len(X) > 0:
         top = heapq.heappop(X)
@@ -53,6 +104,8 @@ def solution(screen):
     
         
 def main():
+    tkwindow()
+    instruction()
     screen = pygame.display.set_mode((screenX,screenY))
     pygame.display.set_caption("SHORTEST PATH")
     screen.fill(WHITE)
@@ -82,11 +135,12 @@ def main():
                 color = screen.get_at((mouse_x,mouse_y))
                 # print(color)
         button = pygame.key.get_pressed()
-        if button[pygame.K_UP]:
+        if button[pygame.K_SPACE]:
             solution(screen)
             run = 0
-    pygame.time.delay(5000)
+    pygame.time.delay(500)
     pygame.display.quit()
     pygame.quit()
+    answer()
      
 main()
